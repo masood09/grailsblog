c610f533087a0535032b24f579baa97c34b2bd46
Masood Ahmed <masood.ahmed09@gmail.com>
2013-11-23 18:29:10 +0530

    Adding User domain class properties and writing test to check for them.
    Also writing a function which returns user’s fullname.

--- grails-app/domain/grailsblog/User.groovy
+++ grails-app/domain/grailsblog/User.groovy
@@ -1,7 +1,30 @@
 package grailsblog
 
 class User {
+    String firstname
+    String middlename
+    String lastname
+    String nickname
+    String email
+    String password
+    Boolean isAdmin
+    Boolean isActive
+    String profilePic
+    Date dateCreated
+    Date lastUpdated
 
     static constraints = {
+    }
+
+    String getFullname() {
+        String fullname
+
+        if (middlename) {
+            fullname = firstname.trim() + " " + middlename.trim() + " " + lastname.trim()
+        } else {
+            fullname = firstname.trim() + " " + lastname.trim()
+        }
+
+        return fullname
     }
 }
