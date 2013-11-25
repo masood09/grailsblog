5b2caf405041171700a77d52d388644f32a17e9f
Masood Ahmed <masood.ahmed09@gmail.com>
2013-11-25 13:39:56 +0530

    SecUser controller to user the url format /admin/users

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,6 +1,9 @@
 class UrlMappings {
 
     static mappings = {
+        "/admin/users"(controller: "SecUser", action: "index")
+        "/admin/users"(resources: 'secUser', excludes: ['show'])
+
         "/$controller/$action?/$id?(.${format})?"{
             constraints {
                 // apply constraints here
